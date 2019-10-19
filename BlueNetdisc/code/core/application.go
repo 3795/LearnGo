4d82f3f0ab8d155d64a44eef80aaf781273a7b32
2471554649 <2471554649@qq.com>
2019-10-19 11:02:16 +0800

    删除项目

--- BlueNetdisc/code/core/application.go
+++
@@ -1,6 +0,0 @@
-package core
-
-type Application interface {
-	// 应用启动入口
-	Start()
-}
--- BlueNetdisc/code/core/global.go
+++
@@ -1,9 +0,0 @@
-package core
-
-var APPLICATION Application
-
-var LOGGER Logger
-
-var CONFIG Config
-
-var CONTEXT Context
