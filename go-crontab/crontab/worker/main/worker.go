49a33e53eac7e4d723d2e101a5b063967c3d28b3
3795 <seven.iwi@gmail.com>
2019-11-11 10:09:29 +0800

    完善worker节点功能

--- go-crontab/crontab/worker/main/worker.go
+++ go-crontab/crontab/worker/main/worker.go
@@ -1,19 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"LearnGo/go-crontab/crontab/worker"
+	"flag"
+	"fmt"
+	"runtime"
+	"time"
+)
 
-func main() {
-	ch1 := make(chan int, 10)
-	for i := 0; i < 11; i++ {
-		append(ch1, i)
-	}
-	fmt.Println("程序未被阻塞")
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	flag.StringVar(&confFile, "config", "./worker.json", "worker.json")
+	flag.Parse()
 }
 
-func append(ch chan int, i int) {
-	select {
-	case ch <- i:
-	default:
-		fmt.Printf("数字 %d 被丢弃了\n", i)
+// 初始化线程数量
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	var (
+		err error
+	)
+
+	initArgs()
+
+	initEnv()
+
+	// 加载配置
+	if err = worker.InitConfig(confFile); err != nil {
+		goto ERR
 	}
+
+	// 服务注册
+	if err = worker.InitRegister(); err != nil {
+		goto ERR
+	}
+
+	// 启动日志协程
+	if err = worker.InitLogSink(); err != nil {
+		goto ERR
+	}
+
+	// 启动执行器
+	if err = worker.InitExecutor(); err != nil {
+		goto ERR
+	}
+
+	// 启动调度器
+	if err = worker.InitScheduler(); err != nil {
+		goto ERR
+	}
+
+	// 初始化任务管理器
+	if err = worker.InitJobMgr(); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+	return
+
+ERR:
+	fmt.Println(err)
+
 }
